d8b57f43db1fb80952d4cd03f06c6edadf6ae45e
Jack Huang <q994702390@Gmail.com>
2019-10-05 19:08:27 +0800

    add strtoint

--- interview.cpp
+++ interview.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <climits>
 #include <stack>
+#include <algorithm>
 
 struct ListNode {
 	int val;
@@ -13,9 +15,13 @@ class solution
 {
 public:
 	bool Find(int target, std::vector<std::vector<int>> array);	// 剑指Offer 二维数组中的查找
-	void replaceSpace(char *str, int length);		// 剑指Offer 替换空格
 	ListNode* ReverseList(ListNode* head);			// 剑指Offer 倒序打印单链表
-	void ReverseString(std::string& str, size_t n);	// 字符串翻转				
+	void ReverseString(std::string& str, size_t n);	// 字符串翻转	
+	bool FindBrotherStr(std::string str, std::string target);	// 查找兄弟字符串
+	int StrToInt(std::string str);	// 字符串转整数
+	char FindFirstSingleChar(std::string str);	// 查找第一个只出现一次的字符
+	int LongestPalindrome(std::string str);	// 最长回文子串
+	bool isLinkListCross(ListNode* listA, ListNode* listB);	// 判断两个单链表是否相交
 };
 
 bool solution::Find(int target, std::vector<std::vector<int>> array)
@@ -53,9 +59,7 @@ ListNode* solution::ReverseList(ListNode* head)
 		pPrev = pNode;
 		pNode = pNext;
 	}
-
 	return pNewHead;
-	
 }
 
 void solution::ReverseString(std::string &str, size_t n)
@@ -83,12 +87,106 @@ void solution::ReverseString(std::string &str, size_t n)
 	return;
 }
 
+bool solution::FindBrotherStr(std::string str, std::string target)
+{
+	size_t targetLen = target.length();
+	if (targetLen > str.length()) return false;
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		size_t j = i;
+		for (; j < targetLen + i; j++)
+		{
+			if (target.find(str[j]) == std::string::npos) break;
+		}
+		if (j == targetLen + i) return true;
+	}
+	return false;
+}
+
+int solution::StrToInt(std::string str)
+{
+	unsigned int res = 0;
+	bool isMinus = false;
+	const int intMax = int(unsigned(~0) >> 1);
+	const int intMin = -int(unsigned(~0) >> 1) - 1;
+	
+	if (str[0] == '-' || str[0] == '+')
+	{
+		str = str.substr(1, str.length() - 1);
+		str[0] == '-' ? isMinus = true : isMinus = true;
+	}
+
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (!isdigit(str[i])) continue;
+		int temp = str[i] - '0';
+		if (!isMinus && (res > intMax / 10 || (res == intMax / 10 && temp >= intMax % 10)))
+		{
+			res = intMax;
+			break;
+		}
+		if (isMinus && (res > unsigned(intMin) / 10 || (res == unsigned(intMin) / 10 && temp >= unsigned(intMin) % 10)))
+		{
+			res = intMin;
+			break;
+		}
+		res = res * 10 + temp;
+	}
+	return isMinus ? -int(res) : int(res);
+}
+
+
+char solution::FindFirstSingleChar(std::string str)
+{
+	int* list = new int[CHAR_MAX]();
+	char res = '\0';
+	for (size_t i = 0; i < str.length(); i++) (*(list + str[i])) ++;
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (*(list + str[i]) == 1)
+		{
+			res = str[i];
+			break;
+		}
+	}
+	delete[] list;
+	return res;
+}
+
+int solution::LongestPalindrome(std::string str)
+{
+	unsigned int res = 0;
+	size_t strLen = str.length();
+	for (size_t i = 0; i < strLen; i++)
+	{
+		str.insert(i*2, "#");
+	}
+	str = "$" + str + "#";
+	std::cout << str << std::endl;
+	unsigned* PalindromeLen = new unsigned[str.length()]();
+	size_t mx = 0;
+	size_t id = 0;
+	for (size_t i = 1; i < str.length(); i++)
+	{
+		PalindromeLen[int(i)] = mx > i ? std::min(PalindromeLen[int(2 * id - i)], mx - i) : 1;
+		while (str[i + PalindromeLen[i]] == str[i - PalindromeLen[i]]) PalindromeLen[i]++;
+		if (i + PalindromeLen[i] > mx)
+		{
+			mx = i + PalindromeLen[i];
+			id = i;
+		}
+	}
+	for (size_t i = 0; i < str.length(); i++)
+		if (PalindromeLen[int(i)] > res) res = PalindromeLen[i];
+	delete[] PalindromeLen;
+	return res - 1;
+}
+
 
 int main()
 {
 	solution mySolution;
-	std::string test = "helloworld";
-	mySolution.ReverseString(test, 5);
-	std::cout << test << std::endl;
+	std::string src = "12212321";
+	std::cout << mySolution.StrToInt(src) << std::endl;
 	return 0;
 }
